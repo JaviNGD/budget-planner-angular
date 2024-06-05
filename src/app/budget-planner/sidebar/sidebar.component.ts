d6febbe41e985436370f675ce3f662fd0217f6ec
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-06-05 16:46:26 -0400

    Add sidebar component

--- src/app/budget-planner/sidebar/sidebar.component.ts
+++ src/app/budget-planner/sidebar/sidebar.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  isSlideOut = true;
+  constructor(private router: Router) { }
+
+  toggleSidebar() {
+    this.isSlideOut = !this.isSlideOut;
+    console.log('toggleSidebar', this.isSlideOut);
+  }
+
+  onDashboard() {
+    console.log('onDashboard');
+    this.router.navigate(['/budget-planner/dashboard']);
+  }
+
+  onProfile() {
+    console.log('onProfile');
+    this.router.navigate(['/budget-planner/profile']);
+  }
+
+  onHistory() {
+    console.log('onHistory');
+    this.router.navigate(['/budget-planner/history']);
+  }
+
+  onLogout() {
+    console.log('onLogout');
+    this.router.navigate(['/budget-planner/login']);
+  }
 
 }
