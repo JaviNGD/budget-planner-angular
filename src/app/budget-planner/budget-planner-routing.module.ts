ae7a7c9f33dba0a7c23642f6398b80af6512090e
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-06-05 22:56:19 -0400

    Add dashboard component

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
-  {path: 'sidebar', component:SidebarComponent}
+  {path: 'sidebar', component:SidebarComponent},
+  {path: 'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
---
+++ src/app/budget-planner/dashboard/dashboard.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatIconModule, SidebarComponent, CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  // Income data
+  lastMonthsIncome = [
+    {
+      month: 'January',
+      income: '$2000'
+    },
+    {
+      month: 'February',
+      income: '$3000'
+    },
+    {
+      month: 'March',
+      income: '$4000'
+    }
+  ];
+
+  currentMonthIncome = { month: 'April', income: '$5000'};
+
+  // Expenses data
+  lastMonthsExpenses = [
+    {
+      month: 'January',
+      expenses: '$1000'
+    },
+    {
+      month: 'February',
+      expenses: '$2000'
+    },
+    {
+      month: 'March',
+      expenses: '$3000'
+    }
+  ];
+
+  // To Do Transactions 
+  toDoTransactions = [
+      { title: 'Pay Rent', completed: true},
+      { title: 'Pay Electricity Bill', completed: false},
+      { title: 'Pay Internet Bill', completed: false},
+  ];
+
+  currentMonthExpenses = { month: 'April', expenses: '$4000'};
+
+  constructor(private router: Router) {}
+
+  onIncome() {
+    console.log('View Income');
+    this.router.navigate(['/budget-planner/income']);
+  }
+
+  onExpense() {
+    console.log('View Expense');
+    this.router.navigate(['/budget-planner/expense']);
+  }
+
+  onToDo() {
+    console.log('View To Do');
+    this.router.navigate(['/budget-planner/to-do']);
+  }
+}
